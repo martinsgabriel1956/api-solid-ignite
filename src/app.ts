718f80d4226e616b50b0581c277714d1efd313b9
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-10-06 20:03:51 -0300

    feat: Registration of a user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name : "John Doe",
-    email: "uUk9b@example.com",
-  }
-});
+import { z as zod } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
+
+app.post("/users", async (request, response) => {
+  const registerBodySchema = zod.object({
+    name: zod.string(),
+    email: zod.string(),
+    password: zod.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return response.status(201).send();
+});
