cdd1e0920b4fb47c9fa16359831da77be8aad80a
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-10-06 15:21:08 -0300

    feat: Prisma ORM fundamentals

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name : "John Doe",
+    email: "uUk9b@example.com",
+  }
+});
 
 export const app = fastify();
