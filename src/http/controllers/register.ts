e0b424faef734f22eb5be3c21a033062c1a2d6cb
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-10-06 22:36:53 -0300

    feat: Password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z as zod } from "zod";
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 
 export async function register (request: FastifyRequest, response: FastifyReply)  {
   const registerBodySchema = zod.object({
@@ -11,11 +12,25 @@ export async function register (request: FastifyRequest, response: FastifyReply)
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if(userWithSameEmail) {
+    return response.status(409).send({
+      message: "User already exists",
+    });
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash
     },
   });
 
